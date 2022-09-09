f9fc6471c8b0effe6c609c7086faa1b48a4efd5a
Indah Retno Iriani <91863400+IndahRetnoIriani@users.noreply.github.com>
2022-09-09 11:36:30 +0700

    Add files via upload

--- segitiga.java
+++ segitiga.java
@@ -6,50 +6,34 @@
 
 /**
  *
- * @author TOSHIBA
+ * @author Indah Retno Iriani
  */
+import java.io.FileNotFoundException;
+import java.util.Formatter;
 public class segitiga {
     public static void main(String[] args){
+                 try (Formatter file = new Formatter("C:\\Users\\TOSHIBA\\Documents\\NetBeansProjects\\mavenproject4\\src\\main\\java\\sgt.txt")) {
         int[] sgt = {2, 1, 4, 1, 7, 6, 2, 1, 0, 4};
         int count = 0;
-        /*int n = 10;
-            for(int i=0; i<=n; i++) {
-                for(int j=n; j>i; j--) {
-                    System.out.print(" ");
-                }
-                for(int k=1; k<=i; k++) {
-                    System.out.print("* ");
-                }
-                System.out.println();
-            }
-            System.out.println();
-            System.out.print("baris = " + n);*/
-            /*for(int i=1; i<=4; i++) {
-                for(int j=4; j>=i; j--) {
-                    System.out.print(" ");    
-                }
-                for(int k=1; k<=i; k++) {
-                    System.out.print(sgt[count]+" ");
-                    count++;
-                }
-                System.out.println();
-            }*/
  //Segitiga sama sisi array menggunakan do-while
            int i=1;
            do{
                int j=4;
                do{
                    System.out.print(" ");
+                   file.format("%s", " ");
                    j--;
                }while(j>=i);
                int k=1;              
                do{
                    System.out.print(sgt[count]+" ");
+                   file.format("%s", sgt[count] +" ");
                    count++;
                    k++;
                }while (k<=i);
                i++;
                System.out.println();
+               file.format("%s %n","");
            }while(i<=4);
            
  //Penjumlahan array menggunakan for   
@@ -58,23 +42,11 @@ public class segitiga {
                 jum = num + jum;
             }
             System.out.print("Jumlah array adalah " + jum);
-            /*int j = 0;
-            do {
-                int i = 0;
-                        do{
-                             System.out.print(sgt[i]);
-                             i++;
-                            System.out.println(" ");
-                        } while(j <= i);
-                        System.out.println();
-            }while(j<=9);
-                    System.out.println();
-                
-            //}
-            /*for(int i = 0; i<sgt.length; i++){
-                sgt[i] = 2 * i;
-                System.out.print("i");
-            }*/
+            file.format("%s", "Jumlah array adalah " + jum);
+            
+            }catch(FileNotFoundException ex){
+            //Menampilkan pesan jika file tidak ditemukan
+            System.out.println("File Tidak Ditemukan"); 
+        }
     }
-
 }       
